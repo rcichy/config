cef560775a0d543a7114f4d2cd97a4224e8f74cf
Radek Cichy <radek.cichy.it@gmail.com>
2024-09-20 22:19:27 +0200

    Clean-up nvim plugins

---
+++ nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    event = "VeryLazy",
+    version = "*",
+    -- config = true,
+    opts = {
+      size = 10,
+      open_mapping = "<c-s>",
+    },
+  },
+}
